9020881c53bf89b2401fe7f936bb16ea702b725f
shital gawade <shitalgawade131@gmail.com>
2022-12-18 23:51:43 +0530

    Day5

---
+++ Dice.sh
@@ -0,0 +1,22 @@
+
+Dice=$((RANDOM%6))
+case $Dice in
+   6)
+   echo "6"
+   ;;
+   5)
+   echo "5"
+   ;;
+   4)
+   echo "4"
+   ;;
+   3)
+   echo "3"
+    ;;
+    2)
+    echo "2"
+    ;;
+    1)
+    echo "1"
+     ;;
+esac
---
+++ add5.sh
@@ -0,0 +1,11 @@
+echo "enter 5 number"
+read a
+read b
+read c
+read d
+read e
+z=$(( $a + $b + $c + $d + $e ))
+x=$(( $z/5 ))
+echo $z
+echo $x
+
