434886e41120a858b5c2674da02978abf4736760
shital gawade <shitalgawade131@gmail.com>
2022-12-28 19:52:26 +0530

    new branch

--- add5.sh
+++ add5.sh
@@ -1,11 +1,18 @@
-echo "enter 5 number"
-read a
-read b
-read c
-read d
-read e
-z=$(( $a + $b + $c + $d + $e ))
+#!/bin/bash
+
+num1=$(($(($RANDOM%90))+10))
+echo "$num1"
+num2=$(($(($RANDOM%90))+10))
+echo "$num2"
+num3=$(($(($RANDOM%90))+10))
+echo "$num3"
+num4=$(($(($RANDOM%90))+10))
+echo "$num4"
+num5=$(($(($RANDOM%90))+10))
+echo "$num5"
+
+z=$(( $num1 + $num2 + $num3 + $num4 + $num5 ))
 x=$(( $z/5 ))
-echo $z
-echo $x
+echo "$z"
+echo "$x"
 
---
+++ inchToFeet.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Enter Ditsance in inch"
+read i
+
+f=$(( $i/12 ))
+
+echo "Distance in feet: $f"
